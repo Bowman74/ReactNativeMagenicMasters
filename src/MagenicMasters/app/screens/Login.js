334b18e046c47019f6b851c1f5fad862f68503d6
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-02-06 23:08:13 -0500

    Initial checkin

---
+++ src/MagenicMasters/app/screens/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Got it</Text>
+                <Button title="Hello!" onPress={() => {}} />
+            </View>
+        );
+    }
+}
