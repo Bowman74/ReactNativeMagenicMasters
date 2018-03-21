215e18ebb866b56f8fa083c258b802cb90a6b3c7
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-03-20 21:18:07 -0400

    Added camera to app

--- src/MagenicMasters/android/app/src/main/java/com/magenic/magenicmasters/MainApplication.java
+++ src/MagenicMasters/android/app/src/main/java/com/magenic/magenicmasters/MainApplication.java
@@ -3,6 +3,7 @@ package com.magenic.magenicmasters;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,8 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-              new SQLitePluginPackage(),
-          new MainReactPackage()
+            new SQLitePluginPackage(),
+            new MainReactPackage(),
+            new RNCameraPackage()
       );
     }
 
